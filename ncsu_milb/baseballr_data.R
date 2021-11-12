f7818e8edd61ce7093177cf1dac748428d1be0b3
OminousPotato <67799535+OminousPotato@users.noreply.github.com>
2021-11-11 20:09:13 -0500

    Made getting data a little less garbage

--- ncsu_milb/baseballr_data.R
+++ ncsu_milb/baseballr_data.R
@@ -15,19 +15,41 @@ require(tidyverse)
 
 
 #TODO: Identify relevant date ranges and find a way to use get_game_pks on all dates.
-pB_dateRange <- ""
-pBailey_gmpk <- get_game_pks_mlb(date = pB_dateRange, level_ids = c(11))
+# pB_dateRange <- seq.Date(as.Date('2021-01-01'), 
+#                          as.Date('2021-12-31'), 
+#                          'day')
+# pBailey_gmpk <- get_game_pks_mlb(date = pB_dateRange, level_ids = c(13, 14)) #13: Class-A Advanced, 14: Class A
+# 
+# wW_dateRange <- seq.Date(as.Date('2021-01-01'), 
+#                          as.Date('2021-12-31'), 
+#                          'day')
+# wWilson_gmpk <- get_game_pks_mlb(date = wW_dateRange, level_ids = c(13, 14))
+# #may need to split this one up because it's over multiple seasons
+# 
+# jB_dateRange <- seq.Date(as.Date('2021-01-01'), 
+#                         as.Date('2021-12-31'), 
+#                         'day')
+# jButler_gmpk <-get_game_pks_mlb(date = jB_dateRange, level_ids = c(13))
+# #may need to change 
 
-wW_dateRange <- ""
-wWilson_gmpk <- get_game_pks_mlb(date = wW_dateRange, level_ids = c(11, 12))
-#may need to split this one up because it's over multiple seasons
-
-jB_dateRange <- ""
-jButler_gmpk <-get_game_pks_mlb(date = jB_dateRange, level_ids = c(13, 16))
-#may need to change 
+# gamepacks <- map_df(.x = seq.Date(as.Date('2021-01-01'), 
+#                           as.Date('2021-12-31'), 
+#                           'day'), 
+#             ~get_game_pks_mlb(date = .x, 
+#                               level_ids = c(11,12,13,14,15))
+# )
 
 
 ###################################################################################
 #Get Pitch by Pitch Data for each player.
 ###################################################################################
+
+pBailey <- baseballr::milb_batter_game_logs_fg("sa3014442", year = 2021)
+
+wWilson <- baseballr::milb_batter_game_logs_fg("sa3009871", year = 2021)
+
+jButler <- baseballr::milb_batter_game_logs_fg("sa3017292", year = 2021)
+
+
+
 
